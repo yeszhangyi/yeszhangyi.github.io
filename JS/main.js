a067e11880fa90f2c7e1191b1b1229d0785965d0
yeszhangyi <yeszhangyi@163.com>
2018-01-07 11:20:12 +0800

    01072

--- JS/main.js
+++
@@ -1,7 +0,0 @@
-
-    function show() {   
-        var a = document.getElementById('info').value;  
-        var li = document.createElement('li');  
-        li.innerText = a;  
-        document.getElementById('list').appendChild(li);  
-    }  
