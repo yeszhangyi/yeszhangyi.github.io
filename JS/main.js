d0ecf7ac6fcdea3e3f35a027eeaadfbd0b48be8b
yeszhangyi <yeszhangyi@163.com>
2018-01-07 11:00:03 +0800

    20180107

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,7 @@
-    // 定义函数  
-    function show() {  
-        // 获得输入  
+
+    function show() {   
         var a = document.getElementById('info').value;  
-        // 显示  
-        // document.getElementById('result').innerText = a;  
         var li = document.createElement('li');  
         li.innerText = a;  
-      
         document.getElementById('list').appendChild(li);  
-        // alert('hello');  
     }  
